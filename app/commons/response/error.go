4f30f10e76353af5462ac4fd0e3b0d9ee8f1e934
Naufal Hakim <naufal.arkademi@gmail.com>
2023-05-23 18:03:06 +0700

    create jwt and make response

---
+++ app/commons/response/error.go
@@ -0,0 +1,84 @@
+package response
+
+import "net/http"
+
+type CustomError struct {
+	Code           string      `json:"code"`
+	StatusCode     int         `json:"status_code"`
+	Message        string      `json:"message"`
+	AdditionalInfo interface{} `json:"additional_info,omitempty"`
+}
+
+var (
+	generalError = CustomError{
+		Code:       "ERR0001",
+		StatusCode: http.StatusInternalServerError,
+		Message:    "INTERNAL SERVER ERROR",
+	}
+	repositoryError = CustomError{
+		Code:       "ERR0002",
+		StatusCode: http.StatusInternalServerError,
+		Message:    "REPOSITORY ERROR",
+	}
+	notFoundError = CustomError{
+		Code:       "ERR0003",
+		StatusCode: http.StatusNotFound,
+		Message:    "NOT FOUND ERROR",
+	}
+	unauthorizedError = CustomError{
+		Code:       "ERR0004",
+		StatusCode: http.StatusUnauthorized,
+		Message:    "UNAUTHORIZED",
+	}
+	badRequestError = CustomError{
+		Code:       "ERR0005",
+		StatusCode: http.StatusBadRequest,
+		Message:    "BAD REQUEST ERROR",
+	}
+)
+
+func GeneralError() *CustomError {
+	err := generalError
+	return &err
+}
+func GeneralErrorWithAdditionalInfo(info interface{}) *CustomError {
+	err := generalError
+	err.AdditionalInfo = info
+	return &err
+}
+
+func RepositoryError() *CustomError {
+	err := repositoryError
+	return &err
+}
+
+func RepositoryErrorWithAdditionalInfo(info interface{}) *CustomError {
+	err := repositoryError
+	err.AdditionalInfo = info
+	return &err
+}
+func NotFoundError() *CustomError {
+	err := notFoundError
+	return &err
+}
+
+func NotFoundErrorWithAdditionalInfo(info interface{}) *CustomError {
+	err := repositoryError
+	err.AdditionalInfo = info
+	return &err
+}
+func UnauthorizedError() *CustomError {
+	err := unauthorizedError
+	return &err
+}
+
+func UnauthorizedErrorWithAdditionalInfo(info interface{}) *CustomError {
+	err := unauthorizedError
+	err.AdditionalInfo = info
+	return &err
+}
+
+func BadRequestError() *CustomError {
+	err := badRequestError
+	return &err
+}
